5e0469793bd44b92b66ce8a1c70d5d6a7d1b5dd2
Kirill Kornuta <onemorekorn@gmail.com>
2023-03-31 16:29:27 +0500

    fix burger

--- src/js/script.js
+++ src/js/script.js
@@ -10,11 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
     burger.classList.toggle('burger__active')
     menu.classList.toggle('menu__active')
   })
-
-//   menuList.forEach((item) => {
-//     item.addEventListener('click', () => {
-//       burger.classList.toggle('burger__active')
-//       menu.classList.toggle('menu__active')
-//     })
-//   })
+  menuList.forEach((item) => {
+    item.addEventListener('click', () => {
+      if (menu.classList.contains('menu__active')) {
+        burger.classList.remove('burger__active')
+        menu.classList.remove('menu__active')
+      }
+    })
+  })
 })
