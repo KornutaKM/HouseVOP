158046d26512ae9c8ce131289a0a535b7434a87e
Kirill Kornuta <onemorekorn@gmail.com>
2023-04-01 13:48:40 +0500

    finally House

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,29 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-  let navLink = document.querySelectorAll('.menu__link')
+  let navLink = document.querySelectorAll('.menu__link'),
+    burger = document.querySelector('.burger'),
+    menu = document.querySelector('.menu')
 
   navLink.forEach((link) => {
     link.addEventListener('click', (el) => {
-      navLink.forEach((item) => {
-        item.classList.remove('menu__link_active')
-      })
+      removeActive()
       el.target.classList.toggle('menu__link_active')
     })
   })
+
+  function removeActive() {
+    navLink.forEach((item) => {
+      item.classList.remove('menu__link_active')
+      burger.classList.remove('burger_active')
+      menu.classList.remove('menu_active')
+    })
+  }
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_active')
+	 menu.classList.toggle('menu_active')
+  })
+
+console.log(burger);
 })
